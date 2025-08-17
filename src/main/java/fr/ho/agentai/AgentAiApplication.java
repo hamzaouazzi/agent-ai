12b03a3591d8b1cd2137fa7806082d8c4721adcd
hamzaou <hamzaouazzi97@gmail.com>
2025-08-17 22:30:38 +0200

    :sparkles: add bean simpleVectorStore

--- src/main/java/fr/ho/agentai/AgentAiApplication.java
+++ src/main/java/fr/ho/agentai/AgentAiApplication.java
@@ -1,13 +1,21 @@
 package fr.ho.agentai;
 
+import org.springframework.ai.embedding.EmbeddingModel;
+import org.springframework.ai.vectorstore.SimpleVectorStore;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class AgentAiApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(AgentAiApplication.class, args);
+    }
+
+    @Bean
+    public SimpleVectorStore simpleVectorStore(EmbeddingModel embeddingModel) {
+        return SimpleVectorStore.builder(embeddingModel).build();
     }
 
 }
