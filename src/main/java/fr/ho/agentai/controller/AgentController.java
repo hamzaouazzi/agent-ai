e715bb098ce5ad30fa194d349aca66baf2e27899
hamzaou <hamzaouazzi97@gmail.com>
2025-08-17 22:31:37 +0200

    :sparkles: add loadFile method for uploading file

--- src/main/java/fr/ho/agentai/controller/AgentController.java
+++ src/main/java/fr/ho/agentai/controller/AgentController.java
@@ -1,24 +1,33 @@
 package fr.ho.agentai.controller;
 
 import fr.ho.agentai.agent.AIAgent;
+import fr.ho.agentai.rag.DocumentIndexor;
 import org.springframework.http.MediaType;
-import org.springframework.web.bind.annotation.CrossOrigin;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.multipart.MultipartFile;
 import reactor.core.publisher.Flux;
+
 
 @RestController
 @CrossOrigin(origins = "*")
 public class AgentController {
     private AIAgent aiAgent;
+    private DocumentIndexor indexor;
 
-    public AgentController(AIAgent aiAgent) {
+    public AgentController(AIAgent aiAgent, DocumentIndexor indexor) {
         this.aiAgent = aiAgent;
+        this.indexor = indexor;
     }
 
     @GetMapping(value = "/askAgent", produces = MediaType.TEXT_PLAIN_VALUE)
     public Flux<String> askAgent(@RequestParam(defaultValue = "Hey") String query) {
         return aiAgent.onQuery(query);
     }
+
+    @PostMapping(value = "/loadFile", consumes = MediaType.MULTIPART_FORM_DATA_VALUE, produces = MediaType.MULTIPART_FORM_DATA_VALUE)
+    public void loadFile(@RequestPart("file") MultipartFile file) {
+        indexor.loadFile(file);
+    }
+
+
 }
