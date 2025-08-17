8f2beb11e87a4a36a7b5c6be6c50eb7c4779a57a
hamzaou <hamzaouazzi97@gmail.com>
2025-08-17 22:29:50 +0200

    :sparkles: add defaultAdvisors for chatClient

--- src/main/java/fr/ho/agentai/agent/AIAgent.java
+++ src/main/java/fr/ho/agentai/agent/AIAgent.java
@@ -1,16 +1,31 @@
 package fr.ho.agentai.agent;
 
+import fr.ho.agentai.tool.AgentTools;
 import org.springframework.ai.chat.client.ChatClient;
+import org.springframework.ai.chat.client.advisor.MessageChatMemoryAdvisor;
+import org.springframework.ai.chat.client.advisor.SimpleLoggerAdvisor;
+import org.springframework.ai.chat.client.advisor.vectorstore.QuestionAnswerAdvisor;
+import org.springframework.ai.chat.memory.ChatMemory;
+import org.springframework.ai.vectorstore.SimpleVectorStore;
 import org.springframework.stereotype.Service;
-import org.springframework.web.bind.annotation.RequestParam;
 import reactor.core.publisher.Flux;
 
 @Service
 public class AIAgent {
     private ChatClient chatClient;
 
-    public AIAgent(ChatClient.Builder chatClient) {
-        this.chatClient = chatClient.build();
+    public AIAgent(ChatClient.Builder chatClient,
+                   ChatMemory chatMemory,
+                   AgentTools agentTools,
+                   SimpleVectorStore vectorStore) {
+        this.chatClient = chatClient
+                .defaultAdvisors(
+                        MessageChatMemoryAdvisor.builder(chatMemory).build()
+                )
+                .defaultTools(agentTools)
+                .defaultAdvisors(new SimpleLoggerAdvisor())
+                //.defaultAdvisors(new QuestionAnswerAdvisor(vectorStore))
+                .build();
     }
 
     public Flux<String> onQuery(String query) {
